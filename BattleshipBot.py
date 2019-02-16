809f22711a470427b98dc929b61b80d3463cf430
Mikhail <spyro.yakimenko@gmail.com>
2019-02-16 17:01:04 +0100

    Place the ships

--- BattleshipBot.py
+++ BattleshipBot.py
@@ -10,8 +10,11 @@ ONLINE = True
 
 class Game:
     waitingForCoord = []
+    shipLimit = 5
     def __init__(self, ID):
         self.flag = OFFLINE
+        self.shipCounter = [0, 0]
+        self.ready = [False, False]
         self.playerIds = []
         self.MapPlayer = []
         self.playerIds.append(ID)
@@ -20,12 +23,14 @@ class Game:
             for n in range(5):
                 TempMatrix.append([0]*5)
             self.MapPlayer.append(TempMatrix)
+
     def __del__(self):
         for id in self.playerIds:
             bot.send_message(id, "Игра прервана!")
             idsStates[id] = OFFLINE
             del idsTokens[id]
         #del tokensGame[]
+
     def connect(self, id):
         self.playerIds.append(id)
         for id in self.playerIds:
@@ -33,22 +38,40 @@ class Game:
         self.flag = ONLINE
         self.waitingForCoord.append(self.playerIds[0])
         self.waitingForCoord.append(self.playerIds[1])
+
     def getPlayersIds(self):
         return self.playerIds
+
     def getOtherPlayerId(self, id):
         if self.playerIds[0] == id:
             return self.playerIds[1]
         elif self.playerIds[1] == id:
             return self.playerIds[0]
         return -1
+
     def getIndexOfPlayer(self, PlayerId):
         if self.playerIds[0] == PlayerId:
             return 0
         elif self.playerIds[1] == PlayerId:
             return 1
+
     def createOneSquareShip(self, PlayerId, x, y):
-        self.MapPlayer[self.getIndexOfPlayer(PlayerId)][x][y] = 1
+        PlayerIndex = self.getIndexOfPlayer(PlayerId)
+        if self.ready[PlayerIndex]:
+            bot.send_message(PlayerId, "Вы уже расставили все возможные корабли!")
+        elif self.shipCounter[PlayerIndex] < self.shipLimit:
+            if x >= 0 and x < 5 and y >= 0 and y < 5:
+                if self.MapPlayer[PlayerIndex][x][y] != 1:
+                    self.MapPlayer[PlayerIndex][x][y] = 1
+                    self.shipCounter[PlayerIndex] += 1
+                else:
+                    bot.send_message(PlayerId, "Там уже стоит корабль!")
+
+            if self.shipCounter[PlayerIndex] == self.shipLimit:
+                self.ready[PlayerIndex] = True
+                bot.send_message(self.getOtherPlayerId(PlayerId), "Ваш соперник готов!")
         return
+
     def GetFormattedMap(self, PlayerId):
         resultText = ""
         for y in range(5):
@@ -57,7 +80,7 @@ class Game:
                 if square == 1:
                     resultText += "#"
                 else:
-                    resultText += "."
+                    resultText += "~"
             resultText += "\n"
         return resultText
 
@@ -121,20 +144,30 @@ def Battleships(message):
     #    if message.chat.id in game.getPlayersIds():
     # Establishing a connection between two players
     if PlayerId in waitingForToken:
-        #try:
+        try:
             token = int(message.text)
             establishConnection(token, PlayerId)
-        #except:
-        #    bot.send_message(PlayerId, "Неверный токен!")
+        except:
+            bot.send_message(PlayerId, "Неверный токен!")
     elif idsStates.get(PlayerId) == ONLINE:
-        pass
-    if idsStates.get(PlayerId) == ONLINE:
         if PlayerId in Game.waitingForCoord:
             for s in message.text.split(" "):
+                if len(s) > 2:
+                    bot.send_message(PlayerId, "Неверный ввод!")
+                    return
                 x = ord(s[0]) - 65
+                if x < 0 or x > 4:
+                    x -= 25
+                    if x < 0 or x > 4:
+                        bot.send_message(PlayerId, "Неверный ввод!")
+                        return
                 y = int(s[1]) - 1
+                if y < 0 or y > 4:
+                    bot.send_message(PlayerId, "Неверный ввод!")
+                    return
                 tokensGame[idsTokens[PlayerId]].createOneSquareShip(PlayerId, x, y)
-                bot.send_message(PlayerId, tokensGame[idsTokens[PlayerId]].GetFormattedMap(PlayerId))
+            bot.send_message(PlayerId, tokensGame[idsTokens[PlayerId]].GetFormattedMap(PlayerId))
+
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
